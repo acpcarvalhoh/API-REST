f74dc8b38d268cef2db59afc9baa70d8aa8d63d6
acp <carvalhohuzumak@gmail.com>
2024-04-19 08:52:50 -0300

    prepare to host

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { env } from "./env";
 import { app } from "./app";
 
 app.listen({
-    port: env.PORT
+    port: env.PORT,
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 }).then(() => {
     console.log(`Aplication running on port ${env.PORT}`);
 });
